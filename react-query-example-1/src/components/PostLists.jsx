7ebc85961dde7df793eb09ce1322653a0fb9529f
Pradeep Maurya <aradshub93@gmail.com>
2024-08-18 07:26:57 +0530

    added code for reqct query

--- react-query-example-1/src/components/PostLists.jsx
+++ react-query-example-1/src/components/PostLists.jsx
@@ -1,20 +1,71 @@
-import { useQuery } from "@tanstack/react-query";
-import { fetchPosts } from "../api/api";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { addPost, fetchPosts, fetchTags } from "../api/api";
 import "./PostList.css";
 
 const PostLists = () => {
-  const { data, isError, isLoading, error } = useQuery({
+  const {
+    data: postData,
+    isError,
+    isLoading,
+    error,
+  } = useQuery({
     queryKey: ["posts"],
     queryFn: fetchPosts,
   });
 
+  const { data: tagsData } = useQuery({
+    queryKey: ["tags"],
+    queryFn: fetchTags,
+  });
+
+  // const {
+  //   mutate,
+  //   isError: isPostError,
+  //   isPending,
+  //   error: postError,
+  // } = useMutation({
+  //   mutationFn: addPost,
+  // });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const title = formData.get("title");
+    console.log("title", title);
+    console.log("formData", Array.from(formData.keys));
+  };
+
   console.log("data");
   return (
     <div className="container">
-      {isLoading && <p>Loading...</p>}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="title"
+          className="postbox"
+          placeholder="Enter your post"
+        />
+        <div className="tags">
+          {tagsData?.map((tag) => {
+            return (
+              <div key={tag} className="tag">
+                <input
+                  name={tag}
+                  id={tag}
+                  type="checkbox"
+                  className="checkbox"
+                />
+                <label htmlFor={tag}>{tag}</label>
+              </div>
+            );
+          })}
+        </div>
+        <button className="post_btn">Post</button>
+      </form>
+      {isLoading && <h3>Loading...</h3>}
       {isError && <p>{error?.message}</p>}
-      {data?.length > 0 &&
-        data?.map((post) => {
+      {postData?.length > 0 &&
+        postData?.map((post) => {
           return (
             <div key={post.id} className="post">
               <div>{post.title}</div>
--- react-query-example-1/src/main.jsx
+++ react-query-example-1/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App.jsx";
 import "./index.css";
 
@@ -8,8 +9,9 @@ const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={queryClient} contextSharing={true}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>
 );
